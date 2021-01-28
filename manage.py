a7f11cd7b7e6bce9ccb8695c7d810ebeb153e84c
vincijoy96@gmail.com <vincijoy96@gmail.com>
2021-01-28 12:45:06 +0800

    test docker version change

--- manage.py
+++ manage.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     from django.core.management import execute_from_command_line
     import django
     sys.stdout.write("Django VERSION " + str(django.VERSION) + "\n")
-
+    print("test.version")
     execute_from_command_line(sys.argv)
